6f9796e8a88deed9989733f5db52452056ef3934
osamahamed2 <95640944+osamahamed2@users.noreply.github.com>
2021-12-07 12:33:55 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,53 @@
+
+public class Person {
+    private String name;
+    private int age;
+    private String Datebirth;
+    private String phonenum;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDatebirth() {
+        return Datebirth;
+    }
+
+    public void setDatebirth(String Datebirth) {
+        this.Datebirth = Datebirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/project/java/ProjectJava.java
@@ -0,0 +1,9 @@
+
+package project.java;
+
+public class ProjectJava {
+
+    public static void main(String[] args) {
+        
+    }   
+}
